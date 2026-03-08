87f21842f5876fb97a434212d4d7571a0442f8dc
AellaAinsley <145788726+AellaAinsley@users.noreply.github.com>
2026-03-08 15:30:59 +0800

    redirect back

--- form.php
+++ form.php
@@ -1,5 +1,14 @@
 <?php
-    $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-    $txt = "Name: $_GET["name"]\n Sat breakfast: $_GET["sat_breakfast"]\n"; fwrite($myfile, $txt);
-    fclose($myfile);
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+
+        $name = htmlspecialchars($_GET["name"]);
+        $sat_bf = htmlspecialchars($_GET["sat_breakfast"]);
+
+        $txt = "Name: $name\n Sat breakfast: $sat_bf\n";
+        fwrite($myfile, $txt);
+        fclose($myfile);
+
+        header("Location: index.html");
+    }
 ?>
