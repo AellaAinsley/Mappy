136be2c5bc3506d07bc284355e393c67293dce59
AellaAinsley <145788726+AellaAinsley@users.noreply.github.com>
2026-03-08 15:54:50 +0800

    get to post

--- form.php
+++ form.php
@@ -2,8 +2,8 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST"){
         $myfile = fopen("data.txt", "a");
 
-        $name = htmlspecialchars($_GET["name"]);
-        $sat_bf = htmlspecialchars($_GET["sat_breakfast"]);
+        $name = htmlspecialchars($_POST["name"]);
+        $sat_bf = htmlspecialchars($_POST["sat_breakfast"]);
 
         $txt = "Name: $name\n Sat breakfast: $sat_bf\n";
         fwrite($myfile, $txt);
