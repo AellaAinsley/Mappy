7e62c7f28af905f8eef0e1d735caaa00e8a530bf
AellaAinsley <145788726+AellaAinsley@users.noreply.github.com>
2026-03-08 14:58:19 +0800

    fix

--- form.php
+++ form.php
@@ -1,5 +1,5 @@
 <?php
 $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-$txt = "Name: $_GET["name]\n Sat breakfast: $_GET["sat_breakfast"]\n"; fwrite($myfile, $txt);
+$txt = "Name: $_GET["name"]\n Sat breakfast: $_GET["sat_breakfast"]\n"; fwrite($myfile, $txt);
 fclose($myfile);
 ?>
