9452d37443f00ad13664696c8c29ddbc4cae1671
AellaAinsley <145788726+AellaAinsley@users.noreply.github.com>
2026-03-08 17:20:17 +0800

    fix nav

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function extend(id){
-    var div = document.getElementById(id)
+    var div = document.getElementById(id);
     div.classList.toggle('hid');
+}
+
+function open(id){
+    var div = document.getElementById(id);
+    div.setAttribute("display", "block");
 }
